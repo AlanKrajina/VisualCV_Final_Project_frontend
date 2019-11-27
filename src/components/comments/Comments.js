fa6c4d0c7db92b025f66ede422114bc2fa0dcce1
Alan Krajina <alankrajina88@gmail.com>
2019-11-27 22:22:34 +0100

    finished comments show

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -5,8 +5,7 @@ class Comments extends Component {
 
   render() {
     const associatedComments = this.props.comments.filter(
-      comment => comment.blogId === this.props.blogId
-    );
+      comment => comment.relationships.blog.data.id === this.props.blogId);
 
     const renderComments = associatedComments.map( r => {return <Comment key={r.id} comment={r} deleteComment={this.props.deleteComment}/>})
 
