8c6f72525310231ee4a137f5204e5b1fa6995f76
Alan Krajina <alankrajina88@gmail.com>
2020-01-20 16:49:40 +0100

    cors update

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -20,7 +20,7 @@ export const login = (credentials) => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            'Access-Control-Allow-Origin': '*'
+            'Accepts': 'application/json'
           },
           body: JSON.stringify(credentials)
         })                               
@@ -44,7 +44,7 @@ export const getCurrentUser = () => {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          'Access-Control-Allow-Origin': '*'
+          'Accepts': 'application/json'
         },
       })
         .then(r => r.json())
